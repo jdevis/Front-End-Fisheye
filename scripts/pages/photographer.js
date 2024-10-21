26cba851469b32cb20a5ca11778d1a270e5dae13
jdevis <josephine.roubertie@gmail.com>
2024-10-21 17:03:18 +0200

    begin display data on photographer detail page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,12 +9,27 @@ async function getPhotographerInfos(id) {
 	);
 	return { photographerCard, photographerMedias };
 }
+async function displayData(photographers) {
+	const photographersHeader = document.querySelector(".photograph_header");
+	const photographersSection = document.querySelector(".photograph_medias");
+
+	photographers.photographerCard.forEach((photographer) => {
+		const photographerModel = photographerPage(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersHeader.appendChild(userCardDOM);
+	});
+	photographers.photographerMedias.forEach((photographer) => {
+		const photographerModel = photographerMedias(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(userCardDOM);
+	});
+}
 
 async function init() {
 	let params = new URL(document.location).searchParams;
 	let id = parseInt(params.get("id"));
 
 	const photographerInfos = await getPhotographerInfos(id);
-	//console.log(photographerInfos);
+	displayData(photographerInfos);
 }
 init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -9,6 +9,63 @@ function appendChilds(el, attrs) {
 		el.appendChild(attrs[key]);
 	}
 }
+function photographerPage(data) {
+	const { name, portrait, tagline, city, country, price } = data;
+	const picture = `assets/photographers/${portrait}`;
+
+	function getUserCardDOM() {
+		const h2 = document.createElement("h2");
+		h2.textContent = name;
+		const location = document.createElement("p");
+		location.setAttribute("class", "location");
+		location.textContent = city + ", " + country;
+		const intro = document.createElement("p");
+		intro.textContent = tagline;
+		const divIntro = document.createElement("div");
+		appendChilds(divIntro, { h2, location, intro });
+		const button = document.createElement("buttton");
+		button.textContent = "Contactez-moi";
+		setAttributes(button, {
+			class: "contact_button",
+			onClick: "displayModal()",
+		});
+		// const dayPrice = document.createElement("p");
+		// dayPrice.setAttribute("class", "price");
+		// dayPrice.textContent = price + "€/jour";
+		const img = document.createElement("img");
+		setAttributes(img, { src: picture, alt: "portrait de " + name });
+		const divimg = document.createElement("div");
+		divimg.setAttribute("class", "portrait");
+		divimg.appendChild(img);
+		const div2 = document.createElement("div");
+		div2.setAttribute("class", "intro");
+		appendChilds(div2, { divIntro, button, divimg });
+		return div2;
+	}
+	return { getUserCardDOM };
+}
+
+function photographerMedias(data) {
+	const { date, id, photographerId, image, likes, price, title } = data;
+	const picture = `assets/photographers/${photographerId}/${image}`;
+	function getUserCardDOM() {
+		const article = document.createElement("article");
+		const img = document.createElement("img");
+		setAttributes(img, { src: picture, alt: title });
+		const link = document.createElement("a");
+		link.appendChild(img);
+		const mediaTitle = document.createElement("p");
+		mediaTitle.textContent = title;
+		mediaTitle.setAttribute("class", "title");
+		setAttributes(link, { href: "#", title: "Ouverture du carousel" });
+		const nbreLikes = document.createElement("span");
+		nbreLikes.textContent = likes;
+		setAttributes(nbreLikes, { class: "likes" });
+		appendChilds(article, { link, mediaTitle, nbreLikes });
+		return article;
+	}
+	return { getUserCardDOM };
+}
 
 function photographerTemplate(data) {
 	const { name, portrait, tagline, city, country, price, id } = data;
