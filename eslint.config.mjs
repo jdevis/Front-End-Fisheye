e3ae92a0651a7e5baba04cd48717ce22c21a0eb5
jdevis <josephine.roubertie@gmail.com>
2024-12-02 11:53:48 +0100

    configure ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,29 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginImport from "eslint-plugin-import";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+	{
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "script",
+			},
+		},
+		plugins: {
+			import: pluginImport,
+		},
+		settings: {
+			"import/resolver": {
+				node: {
+					extensions: [".js", ".jsx", ".ts", ".tsx"], // Extensions de fichiers autorisées
+				},
+			},
+		},
+		rules: {
+			"import/no-unresolved": "error",
+		},
+	},
+	pluginJs.configs.recommended,
 ];
